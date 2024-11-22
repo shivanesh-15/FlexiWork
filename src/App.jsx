7af25e4de9f96e02842e5b8f089801ce110cca18
nehas1112 <nehaspam77@gmail.com>
2024-11-22 19:39:38 +0530

    Add files via upload
    pushed signup-page code

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import SignUpPage from "./SignUpPage";
+import Footer from "./Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <SignUpPage />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
